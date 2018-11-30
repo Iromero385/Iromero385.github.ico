34183a2c255733dd45a2ff5b4113997dfed2a787
iromero385 <iromero385@berkeley.edu>
2018-11-29 17:35:08 -0800

    Updates

---
+++ precache-manifest.0bd3dfbef5977c9fbac1a444b0fe6c71.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9aedd68f74bd83de41ea0ad22936799",
+    "url": "/static/media/WireFrame.d9aedd68.png"
+  },
+  {
+    "revision": "b15643879dacbb6d1916",
+    "url": "/static/css/main.7f30c985.chunk.css"
+  },
+  {
+    "revision": "f9d087e8e2944b9ea32c",
+    "url": "/static/js/1.f9d087e8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "576571c96f3e34abc9cf1ef6586d17a0",
+    "url": "/static/media/IMG_0452.576571c9.JPG"
+  },
+  {
+    "revision": "a8552979bb08d3d33b0f9eb012ba916f",
+    "url": "/static/media/Berkeley.a8552979.png"
+  },
+  {
+    "revision": "886ef309e414fda6d46f7a3e483c1f2f",
+    "url": "/static/media/TriviaGame.886ef309.png"
+  },
+  {
+    "revision": "1ee20332b5c47a75a7c42265791fada5",
+    "url": "/static/media/Giftastic.1ee20332.png"
+  },
+  {
+    "revision": "17d6d8d6389dd225861e5ca8aa506d3a",
+    "url": "/static/media/BasicPortfolio.17d6d8d6.png"
+  },
+  {
+    "revision": "3d585c4a1b75b136c19b53e54c7ca69e",
+    "url": "/static/media/JobNinja.3d585c4a.png"
+  },
+  {
+    "revision": "8502c3c0e5a54a7daf4a9cf8c9e9bc67",
+    "url": "/static/media/Psychic.8502c3c0.png"
+  },
+  {
+    "revision": "390cfcd84bf140c66d7d0428918381c6",
+    "url": "/static/media/BootstrapPortfolio.390cfcd8.png"
+  },
+  {
+    "revision": "b15643879dacbb6d1916",
+    "url": "/static/js/main.b1564387.chunk.js"
+  },
+  {
+    "revision": "f7566aa78b9d86b69109b1ac195ef810",
+    "url": "/static/media/ClickyGame.f7566aa7.png"
+  },
+  {
+    "revision": "27e8f7fe24e6ba133e60c008de213ab4",
+    "url": "/static/media/Bamazon.27e8f7fe.png"
+  },
+  {
+    "revision": "3dd9c83f4adba5d1730cca726f19ef02",
+    "url": "/static/media/liri.3dd9c83f.png"
+  },
+  {
+    "revision": "5b318e01799069b9d6be3253e85c66d0",
+    "url": "/static/media/hangman.5b318e01.png"
+  },
+  {
+    "revision": "e852496ca65010ef6dee66a64d731904",
+    "url": "/static/media/TrainSchedule.e852496c.png"
+  },
+  {
+    "revision": "583026206da22db64f917306d277c536",
+    "url": "/static/media/Burger.58302620.png"
+  },
+  {
+    "revision": "021bcace89e67734325b3c9b17bfdfba",
+    "url": "/static/media/CrystalGame.021bcace.png"
+  },
+  {
+    "revision": "4d8cee944766394d36e36f53ee0c95a7",
+    "url": "/static/media/Boom.4d8cee94.png"
+  },
+  {
+    "revision": "4d77db0ec7185174b27861e7ae8b0721",
+    "url": "/static/media/GarageGoods.4d77db0e.png"
+  },
+  {
+    "revision": "449928548a9ba435e9c86e9b97fb14d4",
+    "url": "/static/media/ArticleBlog.44992854.png"
+  },
+  {
+    "revision": "6986c440cf9173ee264090311a610c39",
+    "url": "/index.html"
+  }
+];
